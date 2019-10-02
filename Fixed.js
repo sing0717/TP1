ae222477f94061a72d829d3c5e90ee28323a3762
sing0717 <55978497+sing0717@users.noreply.github.com>
2019-10-02 12:39:46 +0900

    First Minor Fixes

--- Fixed.js
+++ Fixed.js
@@ -13,27 +13,33 @@ function makeRail(LineColor){
 
     nodes.add = function(obj){nodes.push(obj);};
     nodes.curve = function(name){opts.push(name);};
+    nodes.getNode = function(name){
+        for(var i = 0; i < nodes.length; i++){
+            if(nodes[i].name == name)
+                return nodes[i];
+        }
+        return null;
+    };
+
     nodes.connect = function(){
         var i, j = 0;
         for(i = 0;i<nodes.length - 1;i++)
             if(typeof opts[j] !== 'undefined' && opts[j] == nodes[i].name)
-                lines.push(makeLine(LineColor, nodes[i], nodes[i+1], true));
+                lines.push(makeLine(nodes[i], nodes[i+1], true));
             else
-                lines.push(makeLine(LineColor,nodes[i],nodes[i+1], false));
+                lines.push(makeLine(nodes[i],nodes[i+1], false));
     };
     nodes.draw = function(){
         var i;
-        for(i = 0;i<lines.length;i++)
+        for(i = 0;i<lines.length;i++){
+            lines[i].redraw(LineColor, 5);
             canvas.add(lines[i]);
+        }
         for(i = 0 ;i < nodes.length;i++)
             canvas.add(nodes[i], nodes[i].Text);
     };
     
     return nodes;
-}
-
-//textCreator for circle
-function makeText(circle){
 }
 
 //Easier Circle
@@ -47,11 +53,13 @@ function makeCircle(x, y, name) {
         stroke: 'gray',//테두리의 색
         lockMovementX: true,
         lockMovementY: true,//움직이지 않게 고정
+        selectable : false
     });
     c.hasControls = c.hasBorders = false;
     c.selected = false;
     c.name = name;
-    c.Text = new fabric.Text(name, {left: x+30, top: y, fontSize: 20});
+    c.Text = new fabric.Text(name, {left: x+30, top: y, fontSize: 20, lockMovementX: true, lockMovementY: true, selectable: false});
+    c.Text.hasControls = false;
 
     c.on('mousedown', function(){
         if (canvas.startSelected == null) {
@@ -85,39 +93,55 @@ function makeCircle(x, y, name) {
 
 
 //easier makeline
-function makeLine(LineColor,startCircle, endCircle, curve) {
+function makeLine(startCircle, endCircle, curve) {
     var info =  {
-        fill: LineColor,//선의 색
-        stroke: LineColor,//선 테두리의 색
-        strokeWidth: 5,//선 테두리의 굵기
         evented: false,
         lockMovementX: true,
-        lockMovementY: true,//움직이지 않게 고정
+        lockMovementY: true, //움직이지 않게 고정
+        selectable : false
     };
 
-    if(!curve)
-        return new fabric.Line(new Array(startCircle.left, startCircle.top, endCircle.left, endCircle.top), info);
-    info.fill = '';
-    curvePoint = String(new Array(startCircle.left, endCircle.top));
-    return new fabric.Path(String('M '+startCircle.toString() + ' C ' +curvePoint+' '+curvePoint+ ' ' + endCircle.toString()), info);
+    var Line;
+
+    if(!curve){
+        Line = new fabric.Line(new Array(startCircle.left, startCircle.top, endCircle.left, endCircle.top), info);
+    }else{
+        curvePoint = String(new Array(startCircle.left, endCircle.top));
+        Line = new fabric.Path(String('M '+startCircle.toString() + ' C ' +curvePoint+' '+curvePoint+ ' ' + endCircle.toString()), info);
+    }
+    Line.paint = function(color){Line.set('fill', color);if(curve){Line.set('fill','');}Line.set('stroke',color);};
+    Line.resize = function(size){Line.set('strokeWidth', size);};
+    Line.redraw = function(color, size){Line.paint(color);Line.resize(size);};
+    Line.hasControls = false;
+    return Line;
 }
 
 
 //MAIN
 var canvas = makeCanvas("c");
 
-rail = makeRail('red');
-rail.add(makeCircle(200, 30, "R1"));
-rail.add(makeCircle(200, 80, "R2"));
-rail.add(makeCircle(200, 140, "R3"));
-rail.add(makeCircle(200, 180, "R4"));
-rail.add(makeCircle(200, 280, "R5"));
-rail.add(makeCircle(200, 405, "R6"));
-rail.add(makeCircle(300, 505, "R7"));
-rail.add(makeCircle(475, 505, "R8"));
-rail.add(makeCircle(635, 505, "R9"));
+redRail = makeRail('red');
+greenRail = makeRail('green');
+blueRail = makeRail('blue');
 
-rail.curve('R6'); //R6에서 다음 정거장으로 갈때 굽어짐
+redRail.add(makeCircle(400, 30, "R1"));
+redRail.add(makeCircle(400, 80, "R2"));
+redRail.add(makeCircle(400, 140, "R3"));
+redRail.add(makeCircle(400, 180, "R4"));
+redRail.add(makeCircle(400, 280, "R5"));
+redRail.add(makeCircle(400, 405, "R6"));
+redRail.add(makeCircle(500, 505, "R7"));
+redRail.add(makeCircle(675, 505, "R8"));
+redRail.add(makeCircle(835, 505, "R9"));
+redRail.curve('R6'); //R6에서 다음 정거장으로 갈때 굽어짐
 
-rail.connect();
-rail.draw();
+greenRail.add(makeCircle(200, 40, "G1"));
+greenRail.add(makeCircle(200, 140, "G2"));
+greenRail.add(makeCircle(200, 270, "G3"));
+greenRail.add(makeCircle(200, 405, "G4"));
+greenRail.add(makeCircle(200, 505, "G5"));
+
+greenRail.connect();
+redRail.connect();
+greenRail.draw();
+redRail.draw();
