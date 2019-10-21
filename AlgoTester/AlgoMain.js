d2a12a36cd7baa6031a5c153edd4ad12df5a2c5f
lahoo79 <lahoo79@naver.com>
2019-10-21 16:43:14 +0900

    1

--- AlgoTester/AlgoMain.js
+++ AlgoTester/AlgoMain.js
@@ -1,37 +1,102 @@
 var graph = new Graph();
 
-graph.insertVertex('T1');
-graph.insertVertex('R3');
-graph.insertVertex('R1');
-graph.insertVertex('R2');
+graph.insertVertex('A1');
+graph.insertVertex('A2');
+graph.insertVertex('A3');
+graph.insertVertex('A4');
 
-graph.insertVertex('R4');
-graph.insertVertex('R5');
-graph.insertVertex('R6');
-graph.insertVertex('R7'); 
-graph.insertVertex('R8');
+graph.insertVertex('B1');
+graph.insertVertex('B2');
+graph.insertVertex('B3');
+graph.insertVertex('B4'); 
+graph.insertVertex('B5');
+graph.insertVertex('B6');
+graph.insertVertex('B7');
+graph.insertVertex('B8');
+
+graph.insertVertex('C1');
+graph.insertVertex('C2');
+
+graph.insertVertex('D1');
+graph.insertVertex('D2');
+graph.insertVertex('D3');
+
+graph.insertVertex('E1');
+graph.insertVertex('E2');
+graph.insertVertex('E3');
+graph.insertVertex('E4');
+
+graph.insertVertex('F1');
+graph.insertVertex('F2');
+graph.insertVertex('F3');
+graph.insertVertex('F4');
+graph.insertVertex('F5');
 
 graph.insertVertex('G1');
 graph.insertVertex('G2');
 graph.insertVertex('G3');
 graph.insertVertex('G4');
-graph.insertVertex('G5');
-graph.insertVertex('G6');
 
-insertTwoWayArc(graph, 1, 'R1', 'R2');
-insertTwoWayArc(graph, 1.2, 'R2', 'T1');
-insertTwoWayArc(graph, 1.2, 'T1', 'R3');
-insertTwoWayArc(graph, 0.8, 'R3', 'R4');
-insertTwoWayArc(graph, 2, 'R4', 'R5');
-insertTwoWayArc(graph, 2.5, 'R5', 'R6');
-insertTwoWayArc(graph, 2.5, 'R6', 'R7');
-insertTwoWayArc(graph, 2.5, 'R7', 'R8');
+graph.insertVertex('T1');
+graph.insertVertex('T2');
+graph.insertVertex('T3');
+graph.insertVertex('T4');
+graph.insertVertex('T5');
+graph.insertVertex('T6');
+graph.insertVertex('T7');
+graph.insertVertex('T8');
+graph.insertVertex('T9');
+graph.insertVertex('T10');
 
+insertTwoWayArc(graph, 2, 'A1', 'A2');
+insertTwoWayArc(graph, 3, 'A2', 'T1');
+insertTwoWayArc(graph, 2, 'T1', 'T2');
+insertTwoWayArc(graph, 3.2, 'T2', 'A3');
+insertTwoWayArc(graph, 3.3, 'A3', 'T3');
+insertTwoWayArc(graph, 2.8, 'T3', 'T7');
+insertTwoWayArc(graph, 2, 'T7', 'T9');
+insertTwoWayArc(graph, 1, 'T9', 'A4');
 
-insertTwoWayArc(graph, 0.6, 'G1', 'G2');
-insertTwoWayArc(graph, 2.2, 'G2', 'G3');
-insertTwoWayArc(graph, 2.2, 'G3', 'T1');
-insertTwoWayArc(graph, 2.2, 'T1', 'G4');
-insertTwoWayArc(graph, 2, 'G4', 'G5');
-insertTwoWayArc(graph, 2, 'G5', 'G6');
+insertTwoWayArc(graph, 2.7, 'B1', 'T2');
+insertTwoWayArc(graph, 2.2, 'T2', 'B2');
+insertTwoWayArc(graph, 2, 'B2', 'B3');
+insertTwoWayArc(graph, 2.3, 'B3', 'B5');
+insertTwoWayArc(graph, 1.5, 'B3', 'B4');
+insertTwoWayArc(graph, 1.3, 'B4', 'T8');
+insertTwoWayArc(graph, 2.5, 'B5', 'T3');
+insertTwoWayArc(graph, 2.6, 'T3', 'B6');
+insertTwoWayArc(graph, 3, 'B6', 'T4');
+insertTwoWayArc(graph, 1, 'T4', 'B7');
+insertTwoWayArc(graph, 1.5, 'B7', 'B8');
+insertTwoWayArc(graph, 2, 'B8', 'B1');
 
+insertTwoWayArc(graph, 1.7, 'T2', 'C1');
+insertTwoWayArc(graph, 2, 'C1', 'C2');
+insertTwoWayArc(graph, 1.5, 'C2', 'T4');
+insertTwoWayArc(graph, 1, 'T4', 'T5');
+
+insertTwoWayArc(graph, 2.6, 'D1', 'D2');
+insertTwoWayArc(graph, 2, 'D2', 'T5');
+insertTwoWayArc(graph, 1, 'T5', 'T6');
+insertTwoWayArc(graph, 1.6, 'T6', 'D3');
+insertTwoWayArc(graph, 3, 'D3', 'T7');
+
+insertTwoWayArc(graph, 2.6, 'T6', 'E1');
+insertTwoWayArc(graph, 3, 'E1', 'E2');
+insertTwoWayArc(graph, 4, 'E2', 'E3');
+insertTwoWayArc(graph, 3, 'E3', 'T10');
+insertTwoWayArc(graph, 2, 'T10', 'E4');
+
+insertTwoWayArc(graph, 4, 'T1', 'F1');
+insertTwoWayArc(graph, 4, 'F1', 'F2');
+insertTwoWayArc(graph, 2, 'F2', 'T8');
+insertTwoWayArc(graph, 2.5, 'T8', 'F3');
+insertTwoWayArc(graph, 2.5, 'F3', 'F4');
+insertTwoWayArc(graph, 4, 'F4', 'F5');
+
+insertTwoWayArc(graph, 2, 'G1', 'T8');
+insertTwoWayArc(graph, 2.9, 'T8', 'G2');
+insertTwoWayArc(graph, 2.7, 'G2', 'T9');
+insertTwoWayArc(graph, 2, 'T9', 'T10');
+insertTwoWayArc(graph, 2, 'T10', 'G3');
+insertTwoWayArc(graph, 3, 'G3', 'G4');
--- scripts/canvasMain.js
+++ scripts/canvasMain.js
@@ -18,66 +18,119 @@ greenRail = makeRail('green');
 blueRail = makeRail('blue');
 yellowRail = makeRail('yellow');
 brownRail = makeRail('brown');
-
+orangeRail = makeRail('orange');
+greenRail = makeRail('green');
+grayRail = makeRail('gray');
 
 Rails.push(greenRail);
 Rails.push(blueRail);
 Rails.push(redRail);
 Rails.push(yellowRail);
 Rails.push(brownRail);
+Rails.push(orangeRail);
+Rails.push(greenRail);
+Rails.push(grayRail);
 
-transfers.push(makeCircle(420,170, "T2"));
-transfers.push(makeCircle(220,500, "T4"));
-transfers.push(makeCircle(200,600, "T5"));
-transfers.push(makeCircle(300,600, "T6"));
-transfers.push(makeCircle(600,600, "T7"));
-transfers.push(makeCircle(700,670, "T10"));
+transfers.push(makeCircle(380,80, "T1"));
+transfers.push(makeCircle(420,150, "T2"));
+transfers.push(makeCircle(510,470, "T3"));
+transfers.push(makeCircle(220,480, "T4"));
+transfers.push(makeCircle(200,580, "T5"));
+transfers.push(makeCircle(300,580, "T6"));
+transfers.push(makeCircle(600,580, "T7"));
+transfers.push(makeCircle(710,170, "T8"));
+transfers.push(makeCircle(730,580, "T9"));
+transfers.push(makeCircle(750,650, "T10"));
 
-brownRail.add(transfers[0]);
-brownRail.add(makeCircle(400, 270, "C1"));
-//brownRail.curve('C1');
-brownRail.add(makeCircle(340, 370, "C2"));
-brownRail.add(transfers[1]);
-brownRail.add(transfers[2]);
+greenRail.add(makeCircle(70, 80, "A1"));
+greenRail.add(makeCircle(180, 80, "A2"));
+greenRail.add(transfers[0]);
+greenRail.add(transfers[1]);
+greenRail.add(makeCircle(470, 300, "A3"));
+greenRail.add(transfers[2]);
+greenRail.add(transfers[6]);
+greenRail.add(transfers[8]);
+greenRail.add(makeCircle(800, 580, "A4"));
 
-blueRail.add(makeCircle(135, 270, "B8"));
-blueRail.add(makeCircle(135, 425, "B7"));
+blueRail.add(makeCircle(390, 500, "B6"));
+blueRail.add(transfers[2]);
+blueRail.add(makeCircle(590, 360, "B5"));
+blueRail.add(makeCircle(590, 360, "B5"));
+blueRail.add(makeCircle(580, 230, "B3"));
+blueRail.add(makeCircle(650, 200, "B4"));
+blueRail.add(transfers[7]);
+blueRail.add(makeCircle(650, 200, "B4"));
+blueRail.add(makeCircle(580, 230, "B3"));
+blueRail.add(makeCircle(520, 180, "B2"));
 blueRail.add(transfers[1]);
-blueRail.add(makeCircle(390, 520, "B6"));
+blueRail.add(makeCircle(270, 170, "B1"));
+blueRail.add(makeCircle(135, 250, "B8"));
+blueRail.add(makeCircle(135, 405, "B7"));
+blueRail.add(transfers[3]);
+blueRail.add(makeCircle(390, 500, "B6"));
 
-yellowRail.add(transfers[3]);
-yellowRail.add(makeCircle(400, 650, "E1"));
-yellowRail.add(makeCircle(500, 670, "E2"));
-yellowRail.add(makeCircle(590, 680, "E3"));
-yellowRail.add(transfers[5]);
-yellowRail.add(makeCircle(800, 660, "E4"));
-//greenRail.curve('R1'); //R6에서 다음 정거장으로 갈때 굽어짐
+brownRail.add(transfers[1]);
+brownRail.add(makeCircle(400, 250, "C1"));
+//brownRail.curve('C1');
+brownRail.add(makeCircle(340, 350, "C2"));
+brownRail.add(transfers[3]);
+brownRail.add(transfers[4]);
 
-redRail.add(makeCircle(50, 350, "D1"));
-redRail.add(makeCircle(50, 600, "D2"));
+redRail.add(makeCircle(50, 330, "D1"));
+redRail.add(makeCircle(50, 580, "D2"));
 //greenRail.add(makeCircle(300, 140, "G3"));
-redRail.add(transfers[2]);
-redRail.add(transfers[3]);
-redRail.add(makeCircle(450, 600, "D3"));
 redRail.add(transfers[4]);
+redRail.add(transfers[5]);
+redRail.add(makeCircle(450, 580, "D3"));
+redRail.add(transfers[6]);
 
+yellowRail.add(transfers[5]);
+yellowRail.add(makeCircle(400, 630, "E1"));
+yellowRail.add(makeCircle(500, 650, "E2"));
+yellowRail.add(makeCircle(590, 660, "E3"));
+yellowRail.add(transfers[9]);
+yellowRail.add(makeCircle(850, 640, "E4"));
 
+orangeRail.add(transfers[0]);
+orangeRail.add(makeCircle(500, 80, "F1"));
+orangeRail.add(makeCircle(630, 80, "F2"));
+orangeRail.add(transfers[7]);
+orangeRail.add(makeCircle(800, 170, "F3"));
+orangeRail.add(makeCircle(900, 170, "F4"));
+orangeRail.add(makeCircle(900, 370, "F5"));
+
+grayRail.add(makeCircle(710, 20, "G1"));
+grayRail.add(transfers[7]);
+grayRail.add(makeCircle(710, 365, "G2"));
+grayRail.add(transfers[8]);
+grayRail.add(transfers[9]);
+grayRail.add(makeCircle(780, 680, "G3"));
+grayRail.add(makeCircle(940, 680, "G4"));
+
+orangeRail.connect();
 greenRail.connect();
 redRail.connect();
 yellowRail.connect();
 brownRail.connect();
 blueRail.connect();
+greenRail.connect();
+grayRail.connect();
 
+orangeRail.draw();
 greenRail.draw();
 redRail.draw();
 yellowRail.draw();
 brownRail.draw();
 blueRail.draw();
+greenRail.draw();
+grayRail.draw();
 
-var textbox = new fabric.Textbox('', {
+var textbox = new fabric.Textbox('할말', {
   left: 200,
   top: 50,
   width: 300,
   fontSize: 20
 });
 canvas.add(textbox).setActiveObject(textbox);
+
+textbox.setText("형이할말");
