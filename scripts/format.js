e74ecf43f68fc9dc21e5fb02c1433a6c63547f26
sing0717 <55978497+sing0717@users.noreply.github.com>
2019-11-12 14:28:36 +0900

    time options added

--- scripts/format.js
+++ scripts/format.js
@@ -1,5 +1,16 @@
-var width = window.innerWidth;
+window.onload = function(){
+    var hourTag = document.getElementById('hourSelect');
+    var minuteTag = document.getElementById('minuteSelect');
+    var options = '';
 
-function setPosCanvas(){
-    
+    for(var i = 0; i<60; i++){
+        options += '<option>' + i +'분</option>';
+    }
+    minuteTag.innerHTML = options;
+
+    options = '';
+    for(var i = 0; i<25; i++){
+        options += '<option>' + i +'시</option>';
+    }
+    hourTag.innerHTML = options;
 }
